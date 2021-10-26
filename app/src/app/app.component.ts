950e527dfff6b5188c4c87041de5bc5ce7f10f1a
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-10-26 14:15:19 +0200

    added test auth with spotify

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import * as querystring from "query-string";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public title = 'app';
+
+  private client_id: string = "52b5a2676ba940f8922f7b62fe0679c0";
+  private scope: string = "";
+
+  constructor(private currentRoute: ActivatedRoute){}
+
+  public async loginWithSpotify(): Promise<void> {
+    const data = {
+      response_type: 'code',
+      client_id: this.client_id,
+      scope: this.scope,
+      redirect_uri: "http://localhost:4200",
+      state: "veryRandomString123"
+    }
+    
+    window.location.href = "https://accounts.spotify.com/authorize?" + querystring.stringify(data);
+  }
+
+  public async ngOnInit(): Promise<void> {
+    this.currentRoute.queryParams.subscribe((map) => {
+      console.log(map.code)
+    })
+  }
+
+  
 }
