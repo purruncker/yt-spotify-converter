2c60db17318d9f3327edb76049b217b12c870bbd
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-10-28 19:40:22 +0200

    some configuration

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Prevent requests getting blocked by
+  // CORS Policy by allowing every domain (origin)
+  app.enableCors({ origin: "*" })
   await app.listen(3000);
 }
 bootstrap();
