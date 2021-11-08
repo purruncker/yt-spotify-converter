33d89f45a0c0e31da37537adabc5fe28918ec396
StinkyGrandpa <rpurrucker99@gmail.com>
2021-11-08 10:22:47 +0100

    Added Errorpage and HomePAge

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { YtComponent } from './yt/yt.component';
 import { SpotifyComponent } from './spotify/spotify.component';
-import { routingComponents } from './app-routing.module'
+import { routingComponents } from './app-routing.module';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { HomePageComponent } from './home-page/home-page.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    ErrorPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
