b15e4f27df00eedaca2e698e916eb3994a1ddeba
Zitzmann <cedric.zitzmann@gmail.com>
2021-11-09 12:56:37 +0100

    updated home page

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { routingComponents } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
+// TODO: Create about page with listed technologies etc... (especially CockroachDB)
 
 @NgModule({
   declarations: [
