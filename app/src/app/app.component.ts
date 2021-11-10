d4a6b17b3735c7c89e1c5c26d1afc97621569b5f
StinkyGrandpa <92435066+StinkyGrandpa@users.noreply.github.com>
2021-11-10 21:48:11 +0100

    memory leak prevention

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
+import { subscribeOn, take, takeUntil } from 'rxjs/operators';
 import { ErrorDTO } from './dto/error.dto';
 import { HttpErrorService } from './services/http-error.service';
 
@@ -15,10 +16,14 @@ export class AppComponent implements OnInit {
   }
   public error: ErrorDTO = undefined;
 
+
   public async ngOnInit(): Promise<void> {
-    this.errService.initError().subscribe(data => {
+    this.errService.initError().pipe(take(1000)).subscribe(data => {
       this.error = data;
+      console.log(data);
     })
+
+
   }
 
   public async createErr() {
