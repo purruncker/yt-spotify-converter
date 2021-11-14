e3c07067107b7f44d9891609b1ebcf191839e20d
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-11-14 22:59:22 +0100

    updated flowStep for future features

--- app/src/app/model/flow-step.model.ts
+++ app/src/app/model/flow-step.model.ts
@@ -1,8 +1,18 @@
+import { Route } from "@angular/router";
 
 export class FlowStep {
 
     public id: number;
     public title: string;
-    public data?: Record<string, any>;
+    public data?: Record<string, any> = {};
+    public isActive?: boolean = false;
+
+    public allowBack?: boolean = false;
+    public allowNext?: boolean = true;
+
+    // TODO: Implement routing
+
+    public nextRoute?: Route;
+    public backRoute?: Route;
 
 }
