5b74895756456469bb0ace297d7b443aaa5ea0d5
StinkyGrandpa <92435066+StinkyGrandpa@users.noreply.github.com>
2021-11-16 17:56:10 +0100

    added Ep: http://localhost:3000/user-info/spotify

--- api/src/user-info/user-info.controller.ts
+++ api/src/user-info/user-info.controller.ts
@@ -1,17 +1,16 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Headers } from '@nestjs/common';
 import { UserInfoService } from './user-info.service';
 import { CreateUserInfoDto } from './dto/create-user-info.dto';
-import { UpdateUserInfoDto } from './dto/update-user-info.dto';
 
 @Controller('user-info')
 export class UserInfoController {
   constructor(private readonly userInfoService: UserInfoService) { }
 
-  @Get()
+  @Get('spotify')
   getUserInfo(@Headers() headers, @Body() createUserInfoDto: CreateUserInfoDto,
   ) {
-    console.log(headers)
-    return this.userInfoService.getUserInfo(createUserInfoDto);
+    console.log('test')
+    return this.userInfoService.getUserInfo(createUserInfoDto, headers.authorization);
   }
 
 }
